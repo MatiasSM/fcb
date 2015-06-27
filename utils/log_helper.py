8c7d4fe8b4640ad68cc81861783388697dd3a5d7
MatiasSM <elmismocorreo-git@yahoo.com.ar>
2015-06-27 12:29:33 -0300

    Improves deep printing
    Adds support to list, set and dict items deep printing

--- utils/log_helper.py
+++ utils/log_helper.py
@@ -6,17 +6,63 @@ def get_logger_module(module_name):
 def get_logger_for(instance):
     return get_logger_module(instance.__class__.__name__)
 
+_pad_value_fmt_str = "{pad}{value}"
+_pad_field_fmt_str = "{pad}{field}:"
+_pad_field_value_fmt_str = _pad_field_fmt_str + " {value}"
+
+def _level_pad(level):
+    return level * '  '
+
+def _print_dict(inst_dic, level):
+    levels_lines = []
+    level_pad = _level_pad(level)
+    fields_level = level+1
+    fields_level_pad = _level_pad(fields_level)
+    for key, value in inst_dic.items():
+        levels_lines.append(_pad_field_fmt_str.format(pad=level_pad, field="*"))
+        levels_lines.append(_pad_field_fmt_str.format(pad=fields_level_pad, field="key"))
+        levels_lines.extend(_do_deep_print(key, fields_level + 1))
+        levels_lines.append(_pad_field_fmt_str.format(pad=fields_level_pad, field="value"))
+        levels_lines.extend(_do_deep_print(value, fields_level + 1))
+    return levels_lines
+
+def _print_list(inst_list, level):
+    levels_lines = []
+    item_idx = 0
+    level_pad = _level_pad(level)
+    for instance in inst_list:
+        levels_lines.append(_pad_field_fmt_str.format(pad=level_pad, field=item_idx))
+        levels_lines.extend(_do_deep_print(instance, level + 1))
+        item_idx += 1
+    return levels_lines
+
+def _is_plain_type(instance):
+    return isinstance(instance, (bool, int, float, str, unicode)) or instance is None
+
 def _do_deep_print(instance, level):
     levels_lines = []
-    for attr in instance.__dict__.keys():
-        if attr == "log" or attr == "_log":
-            continue
-        val = getattr(instance, attr)
-        if isinstance(val, (bool, int, float, str, unicode, list, dict, set)) or val is None:
-            levels_lines.append("{pad}{field}: {value}".format(pad=level * '  ', field=attr, value=val))
-        else:
-            levels_lines.append("{pad}{field}:".format(pad=level * '  ', field=attr))
-            levels_lines.extend(_do_deep_print(val, level=level+1))
+    level_pad = _level_pad(level)
+
+    if _is_plain_type(instance):
+        levels_lines.append(_pad_value_fmt_str.format(pad=level_pad, value=instance))
+    else:
+        for attr in instance.__dict__.keys():
+            if attr == "log" or attr == "_log":
+                continue
+            val = getattr(instance, attr)
+            if isinstance(val, dict):
+                levels_lines.append(_pad_field_value_fmt_str.format(pad=level_pad, field=attr, value="{"))
+                levels_lines.extend(_print_dict(val, level=level+1))
+                levels_lines.append(_pad_value_fmt_str.format(pad=level_pad, value="}"))
+            elif isinstance(val, (list, set)):
+                levels_lines.append(_pad_field_value_fmt_str.format(pad=level_pad, field=attr, value="["))
+                levels_lines.extend(_print_list(val, level=level+1))
+                levels_lines.append(_pad_value_fmt_str.format(pad=level_pad, value="]"))
+            elif _is_plain_type(val):
+                levels_lines.append(_pad_field_value_fmt_str.format(pad=level_pad, field=attr, value=val))
+            else:
+                levels_lines.append(_pad_field_fmt_str.format(pad=level_pad, field=attr))
+                levels_lines.extend(_do_deep_print(val, level=level+1))
     return levels_lines
 
 # TODO MOVE
